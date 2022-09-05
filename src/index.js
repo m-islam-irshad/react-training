4dd4fac50c428d696454fd17093d56b04229dfde
mi-ibne-irshad <islambinirshad@gmail.com>
2022-09-05 15:47:22 +0500

    jsx practice done also use external javascript variable and functions in the jsx code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import the React and ReactDOM  libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// in past
+// const React = require('react');
+
+// getButtonText = ()=>{
+//   return "Cick On me!";
+// }
+function getButtonText(){
+  return "Click On me!";
+}
+
+
+
+// Create a react component
+const App = ()=>{
+  // const buttonText = "Click Me!"
+  // const buttonText = ["Pakistan", "Zindabad"]
+  const buttonText = {text: "Hi There!"}
+  const style = {backgroundColor: "blue", color: "white"}
+  const labelText = "Enter Your Name:";
+  return (
+    <div>
+    <label className="label" htmlFor="name">{labelText}</label>
+    <input id="name" type="text"/>
+    <button style={style}>
+  
+    {/* {buttonText} */}
+    {buttonText.text}
+    {/* {getButtonText()} */}
+    </button>
+    </div>
+  )
+}
+
+
+
+// Take the react component and show it on the screen
+// ReactDOM.render(<App/>, document.querySelector("#root"))
+
+// Anathor method 
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <>
+    <App/>
+    </>
+  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
